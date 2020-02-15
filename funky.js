909ab1e3cdbd472f8b3c636e1e54df1f69deb684
reubengt <reubengeorgethomas@gmail.com>
2020-02-15 22:51:13 +0000

    add dragging boolean to disable updates when moving without a mousedown

--- funky.js
+++ funky.js
@@ -1,24 +1,28 @@
 const synth = new Tone.Synth().toMaster();
 const pad = document.querySelector(".pad");
 const label = document.querySelector(".label");
-
+let dragging = false;
 // synth.triggerAttack(400);
 
 const down = event => {
-  const x = event.pageX;
+  dragging = true;
+  let x = event.pageX;
   label.textContent = Math.round(x) + "Hz";
   synth.triggerAttack(x);
 };
 
 const up = event => {
+  dragging = false;
   label.textContent = "CLICK + DRAG";
   synth.triggerRelease();
 };
 
 const move = event => {
-  var x = event.pageX;
-  synth.setNote(x);
-  label.innerHTML = Math.round(x) + "Hz";
+  if (dragging) {
+    let x = event.pageX;
+    synth.setNote(x);
+    label.innerHTML = Math.round(x) + "Hz";
+  }
 };
 
 pad.addEventListener("pointerdown", down);
