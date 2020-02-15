e592124e07d57a6725631e0e1e45b8846d74b15a
reubengt <reubengeorgethomas@gmail.com>
2020-02-15 23:08:49 +0000

    modularise dynamic values to separate functions, add mobile touch event support

--- funky.js
+++ funky.js
@@ -4,27 +4,75 @@ const label = document.querySelector(".label");
 let dragging = false;
 // synth.triggerAttack(400);
 
+const getLabelColor = x => {
+  if (dragging) {
+    let fraction = x / window.innerWidth;
+    return "hsl(" + (fraction * 360 + 180) + ", 100%, 50%)";
+  } else return "white";
+};
+
+const getBackgroundColor = x => {
+  if (dragging) {
+    let fraction = x / window.innerWidth;
+    return "hsl(" + fraction * 360 + ", 100%, 50%)";
+  } else return "#222";
+};
+
+const getFrequency = x => {
+  let fraction = x / window.innerWidth;
+  return fraction * 1000;
+};
+
+const getLabel = x => {
+  if (dragging) {
+    var frequency = getFrequency(x);
+    return Math.round(frequency) + "Hz";
+  } else return "CLICK / DRAG";
+};
+
 const down = event => {
+  if (Tone.context.state !== "running") {
+    Tone.context.resume();
+  }
   dragging = true;
   let x = event.pageX;
-  label.textContent = Math.round(x) + "Hz";
-  synth.triggerAttack(x);
+  synth.triggerAttack(getFrequency(x));
+  label.textContent = getLabel(x);
+  label.style.color = getLabelColor(x);
+  pad.style.background = getBackgroundColor(x);
 };
 
 const up = event => {
   dragging = false;
-  label.textContent = "CLICK + DRAG";
   synth.triggerRelease();
+  label.textContent = getLabel();
+  label.style.color = getLabelColor();
+  pad.style.background = getBackgroundColor();
 };
 
 const move = event => {
   if (dragging) {
     let x = event.pageX;
     synth.setNote(x);
-    label.innerHTML = Math.round(x) + "Hz";
+    label.textContent = getLabel(x);
+    label.style.color = getLabelColor(x);
+    pad.style.background = getBackgroundColor(x);
   }
 };
 
-pad.addEventListener("pointerdown", down);
-pad.addEventListener("pointerup", up);
-pad.addEventListener("pointermove", move);
+pad.addEventListener("mousedown", down);
+pad.addEventListener("mouseup", up);
+pad.addEventListener("mousemove", move);
+
+pad.addEventListener("touchstart", event => {
+  event.preventDefault();
+  down(event.changedTouches[0]);
+});
+pad.addEventListener("touchend", event => {
+  event.preventDefault();
+  up(event.changedTouches[0]);
+});
+pad.addEventListener("touchmove", event => {
+  event.preventDefault();
+  move(event.changedTouches[0]);
+});
